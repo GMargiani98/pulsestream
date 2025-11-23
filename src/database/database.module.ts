64ae5ccf59da869970bd24a17a4cf1854f591868
Giorgi <g.margiani98@gmail.com>
2025-11-23 09:50:38 +0400

    feat:database module and docker infra

---
+++ src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Global, Module } from '@nestjs/common';
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+import { ConfigService } from '@nestjs/config'; 
+
+export const DB_CONNECTION = 'DB_CONNECTION';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DB_CONNECTION,
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => {
+        return new Kysely<any>({
+          dialect: new PostgresDialect({
+            pool: new Pool({
+              host: configService.getOrThrow<string>('DB_HOST'),
+              port: configService.getOrThrow<number>('DB_PORT'),
+              user: configService.getOrThrow<string>('DB_USER'),
+              password: configService.getOrThrow<string>('DB_PASSWORD'),
+              database: configService.getOrThrow<string>('DB_NAME'),
+            }),
+          }),
+        });
+      },
+    },
+  ],
+  exports: [DB_CONNECTION],
+})
+export class DatabaseModule {}
